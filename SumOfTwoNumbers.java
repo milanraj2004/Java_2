5b3eefdb4d0a2f0bd08bf84037975ccdbd7852c2
milanraj2004 <157353158+milanraj2004@users.noreply.github.com>
2024-09-06 12:31:34 +0530

    Update SumOfTwoNumbers.java

--- SumOfTwoNumbers.java
+++ SumOfTwoNumbers.java
@@ -1,16 +1,5 @@
-import java.util.Scanner;
-
-public class SumOfTwoNumbers {
+public class HelloWorld {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        
-        System.out.print("Enter first number: ");
-        int num1 = sc.nextInt(); // Input first number
-
-        System.out.print("Enter second number: ");
-        int num2 = sc.nextInt(); // Input second number
-
-        int sum = num1 + num2; // Add the numbers
-        System.out.println("Sum: " + sum); // Print the sum
+        System.out.println("Hello, World!"); // Print a message
     }
 }
